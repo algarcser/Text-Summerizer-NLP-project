b644d249a88101dfb22a834fad1ab723cfc36c80
Alejandro Guarnizo de la Torre <algarcser@gmail.com>
2023-11-02 14:48:58 +0100

    building pipeline ingestion. Testing working

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
 
-logger.info("This is a custom message")
 
-
-
-from textSummarizer.constants import *
-print(CONFIG_FILE_PATH)
-
-%pwd
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
